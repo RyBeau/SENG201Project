8e4c4b69b00daf03ccffa8bad321c781b8791855
Daniel Porter <djkporter96@gmail.com>
2019-05-07 10:31:30 +1200

    Changes to ship class

--- src/Main/Ship.java
+++ src/Main/Ship.java
@@ -1,5 +1,20 @@
 package Main;
 
 public class Ship {
+	
+	private int shieldLevel;
+	
+	public Ship() {
+		shieldLevel = 100;
+	}
+	
+	public int getShieldLevel() {
+		return shieldLevel;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
 
 }
